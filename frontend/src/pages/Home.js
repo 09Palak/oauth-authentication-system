1c01be45b7f9367bb1f7a966cc4fdf89e1eea902
09Palak <palaksharmahp16@gmail.com>
2026-03-28 16:07:34 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,7 @@
 export default function Home() {
-  const google = () => window.location.href = "http://localhost:5000/auth/google";
+  const google = () =>
+    window.location.href =
+      "https://oauth-authentication-system.onrender.com/auth/google"; // 👈 FIXED
 
   return (
     <div className="home-container">
@@ -7,7 +9,9 @@ export default function Home() {
         <h1>Welcome</h1>
         <p>Login to continue</p>
 
-        <button className="google-btn" onClick={google}>Continue with Google</button>
+        <button className="google-btn" onClick={google}>
+          Continue with Google
+        </button>
       </div>
     </div>
   );
