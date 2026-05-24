6aa41a0b70608918e19f0af51c0c96c59414930d
09Palak <palaksharmahp16@gmail.com>
2026-05-24 15:17:09 +0530

    Refactor Google Strategy configuration in passport.js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -2,12 +2,10 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/User.js";
 
-// ✅ Serialize
 passport.serializeUser((user, done) => {
   done(null, user._id);
 });
 
-// ✅ Deserialize
 passport.deserializeUser(async (id, done) => {
   try {
     const user = await User.findById(id);
@@ -17,12 +15,11 @@ passport.deserializeUser(async (id, done) => {
   }
 });
 
-// 🔥 GOOGLE STRATEGY (direct values daali hain)
 passport.use(
   new GoogleStrategy(
     {
-      clientID: "322355776615-jrtppkobq32sscoer5l54g14laa7b8sa.apps.googleusercontent.com",          // 👈 yahan daalo
-      clientSecret: "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",  // 👈 yahan daalo
+      clientID: "322355776615-jrtppkobq32sscoer5l54g14laa7b8sa.apps.googleusercontent.com",          
+      clientSecret: "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",  
       callbackURL: "https://oauth-authentication-system.onrender.com/auth/google/callback",
     },
     async (_, __, profile, done) => {
